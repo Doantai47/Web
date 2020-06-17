1cb20f53a85c714a626ddc909ebe98cd899b3e38
Tai <66994879+Doantai47@users.noreply.github.com>
2020-06-17 23:06:18 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+$conn_string ="host=ec2-3-216-129-140.compute-1.amazonaws.com port=5432 database=d3v0o1fi2hlh5m user=eygrieigcqcpfl Password=05003a499b77f8471f36377c85308dd6605a860b6593d7ef424f794a1df31c84";
+$dbconn= pg_connect($conn_string);
+if (isset($_POST['username'])) {
+	# code...
+	$username = $_POST['username'];
+}
+
+if (isset($_POST['pass'])) {
+	# code...
+	$username = $_POST['pass'];
+}
+
+$sql = "SELECT * FROM tblAcount WHERE _user=".$username."' AND _pass='".$pass."'";
+$result =pg_query($dbconn, $sql);
+$row =pg_num_rows($result);
+
+if ($row==1) {
+	echo "Login success";
+}
+else
+{
+	echo "Login failed";
+}
+?>
