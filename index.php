4207d6826ceafd40e2e2b039125f4001c750663d
Tai <66994879+Doantai47@users.noreply.github.com>
2020-06-17 23:05:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="connect.php" method="post" accept-charset="utf-8">
+User: <input type="text" name="username" value="" placeholder="username"><br>
+Password: <input type="password" name="pass" value="" placeholder="******"><br>
+<input type="submit" name="" value="Login">
+</form>
+
+</body>
+</html>
