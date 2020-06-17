10f23807d0e368e30be8b53e4e5f6eb78b98a80d
Tai <66994879+Doantai47@users.noreply.github.com>
2020-06-17 23:45:22 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string ="host=ec2-3-216-129-140.compute-1.amazonaws.com port=5432 database=d3v0o1fi2hlh5m user=eygrieigcqcpfl Password=05003a499b77f8471f36377c85308dd6605a860b6593d7ef424f794a1df31c84";
+$conn_string ="host=ec2-3-216-129-140.compute-1.amazonaws.com port=5432 dbname=d3v0o1fi2hlh5m user=eygrieigcqcpfl password=05003a499b77f8471f36377c85308dd6605a860b6593d7ef424f794a1df31c84";
 $dbconn= pg_connect($conn_string);
 if (isset($_POST['username'])) {
 	# code...
@@ -8,7 +8,7 @@ if (isset($_POST['username'])) {
 
 if (isset($_POST['pass'])) {
 	# code...
-	$password = $_POST['pass'];
+	$pass = $_POST['pass'];
 }
 
 $sql = "SELECT * FROM ACCOUNT WHERE _user=".$username."' AND _pass='".$pass."'";
